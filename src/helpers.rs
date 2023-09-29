99229dfd02a23e5a3ecdff5bc37c1a9d08330162
Maroš Telka <maros@telka.sk>
2023-09-29 20:34:20 +0200

    Fix helpers

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,5 @@
 /// Checks if inserted string contains one of "bad" characters
-pub fn check_character_collision(string: &String) {
+pub fn check_character_collision(string: &str) {
     for char in string.chars() {
         if char == '@' {
             panic!("String cannot contain char \'@\'")
@@ -8,6 +8,6 @@ pub fn check_character_collision(string: &String) {
 }
 
 /// Removes first and last characters if it is inputted character
-pub fn remove_first_and_last_characters<'a>(string: &String, character: char) -> String {
+pub fn remove_first_and_last_characters(string: &str, character: char) -> String {
     string.trim_start_matches(character).trim_end_matches(character).to_owned()
 }
