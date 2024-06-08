d330b082df272c493f856fb8a2664bf1a3cd8039
Maroš Telka <maros@telka.sk>
2024-06-08 14:27:33 +0200

    Remove `from_string()`, Add TryFrom<String> and TryFrom<&str>

--- src/version/segment.rs
+++ src/version/segment.rs
@@ -1,13 +1,6 @@
-use std::{
-    cmp::Ordering,
-    fmt::{Display, Formatter},
-};
+use crate::{version::Version, FMRI};
 use serde::{Deserialize, Serialize};
-use crate::{
-    FMRI,
-    compare::Compare,
-    version::Version,
-};
+use std::fmt::{Display, Formatter};
 
 /// [`Segment`] is a part of [`Version`] in [`FMRI`]
 ///
@@ -28,32 +21,13 @@ use crate::{
 pub struct Segment(Vec<i32>);
 
 impl Segment {
-    /// Parse "1.2.3" into [`Segment`]
-    pub fn from_string(segment: String) -> Self {
-        let str_vec: Vec<&str> = segment.split('.').collect();
-
-        Self(
-            str_vec
-                .iter()
-                .map(
-                    |&s| s.parse::<i32>().unwrap_or_else(|_| panic!("invalid character in {}", s))
-                ).collect()
-        )
-    }
-
     /// Returns [`Segment`] as [String] ("1.2.3")
     pub fn as_string(&self) -> String {
-        let string: &mut String = &mut "".to_owned();
-
-        for (index, segment) in self.get_ref().iter().enumerate() {
-            string.push_str(&segment.to_string() as &str);
-
-            if index + 1 != self.get_ref().len() {
-                string.push('.');
-            }
-        }
-
-        string.clone()
+        self.get_ref()
+            .iter()
+            .map(|segment| segment.to_string())
+            .collect::<Vec<String>>()
+            .join(".")
     }
 
     /// Returns [`Segment`] as [Vec]<[i32]>
@@ -72,70 +46,30 @@ impl Segment {
     }
 }
 
-/// TODO: impl Ord
-/// Implementation of [`Compare`] for [`Segment`]
-impl Compare for Segment {
-    /// Compares version segments
-    ///
-    /// Ordering::Greater == self is newer<br>
-    /// Ordering::Less == self is older<br>
-    /// Ordering::Equal == segments are same<br>
-    ///
-    /// # examples
-    ///
-    /// ```
-    /// use std::cmp::Ordering;
-    /// use fmri::compare::Compare;
-    /// use fmri::version::segment::Segment;
-    /// let a = Segment::from_string("1.0".to_owned());
-    /// let b = Segment::from_string("1.0".to_owned());
-    /// assert_eq!(a.compare(&b), Ordering::Equal);
-    ///
-    /// let a = Segment::from_string("1.1".to_owned());
-    /// let b = Segment::from_string("1.0".to_owned());
-    /// assert_eq!(a.compare(&b), Ordering::Greater);
-    ///
-    /// let a = Segment::from_string("1.0".to_owned());
-    /// let b = Segment::from_string("1.1".to_owned());
-    /// assert_eq!(a.compare(&b), Ordering::Less);
-    /// ```
-    fn compare(&self, comparing_to: &Self) -> Ordering {
-        match self.get_ref().len().cmp(&comparing_to.get_ref().len()) {
-            Ordering::Greater => {
-                for (index, num) in comparing_to.get_ref().iter().enumerate() {
-                    if num > &self.get_ref()[index] {
-                        return Ordering::Less;
-                    } else if num < &self.get_ref()[index] {
-                        return Ordering::Greater;
-                    }
-                }
-                Ordering::Greater
-            }
-            Ordering::Less => {
-                for (index, num) in self.get_ref().iter().enumerate() {
-                    if num > &comparing_to.get_ref()[index] {
-                        return Ordering::Greater;
-                    } else if num < &comparing_to.get_ref()[index] {
-                        return Ordering::Less;
-                    }
-                }
-                Ordering::Less
-            }
-            Ordering::Equal => {
-                for (index, num) in self.get_ref().iter().enumerate() {
-                    if num > &comparing_to.get_ref()[index] {
-                        return Ordering::Greater;
-                    } else if num < &comparing_to.get_ref()[index] {
-                        return Ordering::Less;
-                    }
-                }
-                Ordering::Equal
-            }
-        }
+impl TryFrom<String> for Segment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Self::try_from(&value as &str)
     }
 }
 
-/// Implementation of [`Display`] for [`Segment`]
+impl TryFrom<&str> for Segment {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(Self(
+            value
+                .split('.')
+                .map(|s| {
+                    s.parse::<i32>()
+                        .map_err(|_| format!("invalid character in {}", s))
+                })
+                .collect::<Result<Vec<i32>, String>>()?,
+        ))
+    }
+}
+
 impl Display for Segment {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.as_string())
